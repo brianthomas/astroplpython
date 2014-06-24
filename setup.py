576bd81edfdb7e27e34d84bccad6bcc8f9e28c82
Brian Thomas <bthomas@noao.edu>
2014-06-24 15:42:56 -0400

    further initial construction of package

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(
+    name='AstroPLPython',
+    version='0.1',
+    packages=['astroplpython',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
+
