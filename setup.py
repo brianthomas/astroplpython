5d70270bff969d035222a63d7b6bf745ccd6ff1e
Brian Thomas <bthomas@noao.edu>
2014-06-24 15:46:12 -0400

    update setup with more metadata

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from distutils.core import setup
 setup(
     name='AstroPLPython',
     version='0.1',
+    maintainer='Brian Thomas',
+    maintainer_email='galactictaco@gmail.com',
+    url='https://www.github.com/brianthomas/astroplpython',
     packages=['astroplpython',],
     license='MIT License',
     long_description=open('README.md').read(),
