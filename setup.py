ec0de9fddc5b1043580d82b5ff049e8f4d8891a2
Brian Thomas <bthomas@noao.edu>
2014-07-15 12:58:39 -0400

    try to fix setup.py metadata

--- setup.py
+++ setup.py
@@ -1,14 +1,33 @@
 
-from distutils.core import setup
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages 
 
-setup(
+setup (
+
     name='AstroPLPython',
+    description='PL/Python package for astronomy',
+    url='https://www.github.com/brianthomas/astroplpython',
     version='0.1',
+
+    keywords = 'plpython astronomy',
+    long_description=open('README.md').read(),
+
     maintainer='Brian Thomas',
     maintainer_email='galactictaco@gmail.com',
-    url='https://www.github.com/brianthomas/astroplpython',
-    packages=['astroplpython',],
+
+#    packages=['astroplpython/*', 'tests', 'sql'],
+    packages=find_packages(),
     license='MIT License',
-    long_description=open('README.md').read(),
+
+    package_data = {
+        # If any package contains *.txt or *.md files, include them:
+        '': ['*.txt', '*.md'],
+        # And include any *.sql files found in the sql package, too:
+        'astroplpython': ['*.sql'],
+    },
+
+    test_suite = "astroplpython", 
 )
 
