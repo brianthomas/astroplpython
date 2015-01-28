ec38dff1b628db4fc514c6d893fc747058addc63
Brian Thomas <bthomas@noao.edu>
2015-01-28 14:10:43 -0500

    shift over to tox-based testing

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 
 
 from os.path import dirname, join
-
 from setuptools import setup, find_packages, Command 
 from setuptools.command.test import test as TestCommand
+import os
 
 with open(join(dirname(__file__), 'astroplpython/VERSION'), 'rb') as f:
 	version = f.read().decode('ascii').strip()
@@ -11,22 +11,6 @@ with open(join(dirname(__file__), 'astroplpython/VERSION'), 'rb') as f:
 with open(join(dirname(__file__), 'README.md'), 'rb') as f:
 	long_description = f.read().decode('ascii').strip()
 
-import os
-class PyTest(Command):
-
-    description = "custom test command that uses special run_test.py script built by py.test --genscript"
-
-    user_options = []
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        import sys,subprocess
-        errno = subprocess.call ([sys.executable, 'bin/run_tests.py'])
-        raise SystemExit(errno)
 
 class CleanCommand(Command):
 
@@ -59,16 +43,10 @@ setup (
     packages=find_packages(exclude=('tests', 'tests.*')),
     license='MIT License',
 
-    package_data = {
-        # If any package contains *.txt or *.md files, include them:
-        '': ['*.txt', 'README.md'],
-        # And include any *.sql files found in the sql package, too:
-        'astroplpython': ['*.sql'],
-    },
+    include_package_data=True,
 
     cmdclass = {
 	'distclean': CleanCommand,
-	'test': PyTest
     },
 
 )
