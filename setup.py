9acf245654b3b3b6ffee0f2eade58d22ad6597f2
Brian Thomas <bthomas@noao.edu>
2015-01-28 19:07:55 -0500

    clean up setup, remove old cleancommand in favor of setupext.janitor

--- setup.py
+++ setup.py
@@ -1,30 +1,22 @@
 
-
 from os.path import dirname, join
 from setuptools import setup, find_packages, Command 
-from setuptools.command.test import test as TestCommand
-import os
+
+try:
+    from setupext import janitor
+    CleanCommand = janitor.CleanCommand
+except ImportError:
+    CleanCommand = None
+
+cmd_classes = {}
+if CleanCommand is not None:
+    cmd_classes['clean'] = CleanCommand
 
 with open(join(dirname(__file__), 'astroplpython/VERSION'), 'rb') as f:
 	version = f.read().decode('ascii').strip()
 
 with open(join(dirname(__file__), 'README.md'), 'rb') as f:
 	long_description = f.read().decode('ascii').strip()
-
-class CleanCommand(Command):
-
-    description = "custom clean command that forcefully removes dist/build directories"
-
-    user_options = []
-    def initialize_options(self):
-        self.cwd = None
-
-    def finalize_options(self):
-        self.cwd = os.getcwd()
-
-    def run(self):
-        assert os.getcwd() == self.cwd, 'Must be in package root: %s' % self.cwd
-        os.system('rm -rf ./build ./dist *.egg-info')
 
 setup (
 
@@ -44,12 +36,12 @@ setup (
 
     include_package_data=True,
 
-    cmdclass = {
-	'distclean': CleanCommand,
-    },
+#    setup_requires=['setupext.janitor'],
+    cmdclass = cmd_classes,
 
     install_requires=[
-        'scipy>=0.15.1',
+        'scipy==0.15.1',
     ],
+
 )
 
