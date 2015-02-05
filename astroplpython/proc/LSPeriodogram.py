e4759186bf41a118052a08e138c207f4d845f8c7
Brian Thomas <bthomas@noao.edu>
2015-02-05 16:29:10 -0500

    Implement a minimal LSP calculation as static
    method. Need to sort out the logging, its 'hard-wired'
    more or less in this implementation. there must be
    a better way. Current implementation on toy db results
    in calulation time of about 1.2 ms per object.

--- astroplpython/proc/LSPeriodogram.py
+++ astroplpython/proc/LSPeriodogram.py
@@ -16,10 +16,26 @@ class LSPeriodogram(object):
     '''
     Lomb Scargle Periodogram implementation using scipy.
     '''
-    def pgram (self):
+    @staticmethod
+    def calculate (x_t_list, f_low = 0.01, f_high = 10.0, f_over = 4):
             
-        self._log.debug("pgram() called")
+        import sys
+        logging.basicConfig( stream=sys.stderr )
+        log = logging.getLogger( "astroplpython.proc" )   
+        log.setLevel (logging.WARN)
+         
+        log.debug("LSPeriodogram.calculate() called")
         
+        ' Set instance variables'
+        x = []
+        y = []
+        for v in x_t_list:
+            x.append(v.time)
+            y.append(v.value)
+            
+        x_arr = np.asarray(x)
+        y_arr = np.asarray(y)
+            
         ''' TODO: calculate the periodogram here..
             for now, we will simply mirror back the 
             timeseries information
@@ -27,41 +43,41 @@ class LSPeriodogram(object):
         p_f_list = []
         
         'TODO: calculate this value'
-        f_low = 0.01 
+        #f_low = 0.01 
         'TODO: calculate this value'
-        f_high = 10. 
+        #f_high = 10. 
         
-        self._log.debug("calculate nout")
-        nout = self._f_over * len(self._x)
-       
-        'calculate a list of frequencies'
-        self._log.debug("calculate list of freq")
-        f = np.linspace(f_low, f_high, nout)
+        log.debug("calculate list of frequencies to use")
+        num_out = f_over * len(x)
+#        log.debug(" NUMBER OF BINS OUT:"+str(num_out))
+        f_bins = np.linspace(f_low, f_high, num_out)
         
-        print ("F:"+str(type(f)))
-        print (" shape:"+str(f.shape))
-        
-        'calculate our periodogram'
+        log.debug("calculate pgram")
 #        print("calculate periodogram")
-#        pgram = sp.lombscargle(self._x, self._y, f)
+        pgram = sp.lombscargle(x_arr, y_arr, f_bins)
+        
+        log.debug("PGRAM type:"+str(type(pgram)))
+        log.debug("PGRAM shape:"+str(pgram.shape))
         
         'convert back to form we may use'
-       # i = 0
-       # while (i < len(pgram)): 
-       #     p_f_list.append(p_f(pgram[i],f[i]))
+        for i in range (0, num_out): 
+            p_f_list.append(p_f(pgram[i],f_bins[i]))
+            
+#        for i in range(0, num_out):
+#            p_f_list.append(p_f(1.0, f_bins[i]))
             
         return p_f_list
     
-    ''' 
+''' 
     Set debugging logging on instance.
-    '''
     def debug(self):
         self._log.setLevel( logging.DEBUG )
+'''
         
-    def __init__(self, x_t_list, f_high=2, f_over=4):
-        '''
-        Constructor
-        '''
+'''
+    def __init__(self, x_t_list):
+        
+        print ("xtlist type:"+str(type(x_t_list)))
         
         # create logger
         import sys
@@ -71,24 +87,20 @@ class LSPeriodogram(object):
         ' TODO: validate input parameters'
         
         ' Set instance variables'
-        self._f_high = f_high
-        self._f_over = f_over
-        self._x = []
-        self._y = []
-        
-        #x = []
-        #y = []
-        #for v in x_t_list:
-        #    x.append(v.time)
-        #    y.append(v.value)
+        x = []
+        y = []
+        for v in x_t_list:
+            x.append(v.time)
+            y.append(v.value)
             
  #       self._x = np.array(x, dtype=float) 
  #       print ("X size is:"+len(self._x))
         #normval = self._x.shape[0]
         #self._y = np.array(y, dtype=float) 
-        w = 1.
-        phi = 0.5 * np.pi
+ #       w = 1.
+ #       phi = 0.5 * np.pi
  #       self._y = 10.0 * np.sin(w*x+phi)
  #       print ("Y size is:"+len(self._y))
+ '''
                              
         
