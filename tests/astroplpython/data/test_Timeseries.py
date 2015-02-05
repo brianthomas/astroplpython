2739999c6fa0628e7cfe7a918e3cde3b7d791d66
Brian Thomas <bthomas@noao.edu>
2015-02-05 16:30:31 -0500

    Modify test to match changes in class

--- tests/astroplpython/data/test_Timeseries.py
+++ tests/astroplpython/data/test_Timeseries.py
@@ -17,16 +17,26 @@ class TestTimeseries (unittest.TestCase):
 
 
     def test_strToXTArray (self):
-        import astroplpython.data.Timeseries as t
-        strarr = ['(1,1.)', '(2,2.)', '(2.1,3.)', '(2.018,4.)']
-        x_t_list = t.x_t.strToXTArray(strarr)
-        print (str(x_t_list))
+        
+        import astroplpython.data.Timeseries as Timeseries
+        
+        # test data strarr has combination of integer, floats.
+        #
+        strarr = ['(1,1)', '(2,2.)', '(2.1,3.)', '(2.018,4.)']
+        x_t_list = Timeseries.x_t.strToXTArray(strarr)
+        #print (str(x_t_list))
+        
         self.assertEqual(4, len(x_t_list), "list has right number of elements")
         
-        x = [1, 2, 2.1, 2.018]
-        t = [1., 2., 3., 4.]
+        # Check class, return values. In checking values be sure
+        # to check that we cast back to float
+        x = [1., 2.0, 2.1, 2.018]
+        t = [1.000, 2.0, 3.0, 4.0]
         i = 0
         while (i < 4): 
+            #print (str(x_t_list[i]))
+            #print (str(x_t_list[i].value))
+            self.assertIsInstance(x_t_list[i], Timeseries.x_t, "is class of Timeseries")
             self.assertEquals(x[i], x_t_list[i].value) #, "x value is correct")
             self.assertEquals(t[i], x_t_list[i].time, "t value is correct")
             i += 1
