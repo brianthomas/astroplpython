cbe270a447e01670117d985d1c36fd8fe1f5c39d
Brian Thomas <bthomas@noao.edu>
2015-02-07 14:51:17 -0500

    Scale the mags going in, clean up dead code

--- astroplpython/function/signal/LSPeriodogram.py
+++ astroplpython/function/signal/LSPeriodogram.py
@@ -7,37 +7,41 @@ Created on Jul 11, 2014
 
 from astroplpython.data.PowerFrequencyMeasurement import p_f
 from astroplpython.data.TimeMeasurement import x_t
+import logging
 import scipy.signal as sp 
 import numpy as np
-import logging
 
 class LSPeriodogram(object):
     
-    '''
-    Lomb Scargle Periodogram implementation using scipy.
-    '''
+    TWO_PI = np.pi * 2.0
+    
     @staticmethod
     def calculate (x_t_list, f_low = 0.01, f_high = 10.0, f_over = 4):
+        ''' 
+        Lomb Scargle Periodogram implementation using scipy. 
+        '''
             
-        import sys
-        logging.basicConfig( stream=sys.stderr )
-        log = logging.getLogger( "astroplpython.function" )   
-        log.setLevel (logging.WARN)
+        #import sys
+        #logging.basicConfig( stream=sys.stderr )
+        log = logging.getLogger( "astroplpython.function.signal" )   
+        #log.setLevel (logging.WARN)
          
         log.debug("LSPeriodogram.calculate() called")
         
-        'Prepare the data; convert x_t[] to numpy ndarrays for lsp calc'
+        log.debug("Prepare the data; convert x_t[] to python.numpy and scale");
         x = []
-        y = []
+        t = []
         for v in x_t_list:
             x.append(v.time)
-            y.append(v.value)
+            t.append(v.value)
             
+        # capture values as ndarray and scale
         x_arr = np.asarray(x)
-        y_arr = np.asarray(y)
-            
-        p_f_list = []
+        x_arr = (x_arr-x_arr.mean())/x_arr.std()
         
+        # capture times as ndarray 
+        t_arr = np.asarray(t)
+            
         'TODO: calculate this value ??'
         #f_low = 0.01 
         'TODO: calculate this value ??'
@@ -45,54 +49,21 @@ class LSPeriodogram(object):
         
         log.debug("calculate list of frequencies to use")
         num_out = f_over * len(x)
-#        log.debug(" NUMBER OF BINS OUT:"+str(num_out))
+        log.debug(" Number of frequency bins out:"+str(num_out))
         f_bins = np.linspace(f_low, f_high, num_out)
         
-        log.debug("calculate pgram")
-        pgram = sp.lombscargle(x_arr, y_arr, f_bins)
+        log.debug("Do pgram calculation")
+        pgram = sp.lombscargle(x_arr, t_arr, f_bins)
         
-        log.debug("PGRAM type:"+str(type(pgram)))
-        log.debug("PGRAM shape:"+str(pgram.shape))
-        
-        'convert back to form we may use'
+        log.debug("convert result to form we may use in db, p_f[]"); 
+        p_f_list = []
         for i in range (0, num_out): 
-            p_f_list.append(p_f(pgram[i],f_bins[i]))
+            p_f_list.append(p_f(pgram[i]/LSPeriodogram.TWO_PI,f_bins[i]))
             
+        #log.debug("PGRAM shape:"+str(pgram.shape))
+        
         return p_f_list
     
-''' 
-    Set debugging logging on instance.
-    def debug(self):
-        self._log.setLevel( logging.DEBUG )
-'''
         
-'''
-    def __init__(self, x_t_list):
-        
-        print ("xtlist type:"+str(type(x_t_list)))
-        
-        # create logger
-        import sys
-        logging.basicConfig( stream=sys.stderr )
-        self._log = logging.getLogger( "astroplpython.function" )
-        
-        ' TODO: validate input parameters'
-        
-        ' Set instance variables'
-        x = []
-        y = []
-        for v in x_t_list:
-            x.append(v.time)
-            y.append(v.value)
-            
- #       self._x = np.array(x, dtype=float) 
- #       print ("X size is:"+len(self._x))
-        #normval = self._x.shape[0]
-        #self._y = np.array(y, dtype=float) 
- #       w = 1.
- #       phi = 0.5 * np.pi
- #       self._y = 10.0 * np.sin(w*x+phi)
- #       print ("Y size is:"+len(self._y))
- '''
                              
         
