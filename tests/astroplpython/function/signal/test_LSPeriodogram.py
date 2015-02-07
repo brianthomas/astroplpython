ef6b1a57fe469930075ec3a8996791b51ff33f94
Brian Thomas <bthomas@noao.edu>
2015-02-07 14:57:54 -0500

    Small clean up of dead code in test

--- tests/astroplpython/function/signal/test_LSPeriodogram.py
+++ tests/astroplpython/function/signal/test_LSPeriodogram.py
@@ -4,7 +4,6 @@ Created on Jul 16, 2014
 @author: thomas
 '''
 import unittest
-import logging
 
 from astroplpython.function.signal.LSPeriodogram import LSPeriodogram
 from astroplpython.data.TimeMeasurement import x_t
@@ -21,7 +20,10 @@ class TestLSPeriodogramTestCase (unittest.TestCase):
 
     def test_basic (self):
         
-        logging.getLogger( "astroplpython.proc" ).setLevel(logging.DEBUG)   
+        #logging.basicConfig( stream=sys.stderr ) 
+        #log = logging.getLogger( "astroplpython.function.signal" )
+        #log.setLevel (logging.DEBUG)
+#        logging.getLogger( "astroplpython.function.signal" ).setLevel(logging.DEBUG) 
         
         data = []
         data.append(x_t(0,0))
@@ -29,10 +31,7 @@ class TestLSPeriodogramTestCase (unittest.TestCase):
         data.append(x_t(0,2))
         
         pgram = LSPeriodogram.calculate(data,0.1,10.0,4)
-        ' use debugging logging for tests '
-#        lsp.debug()
         
-#        pgram = lsp.pgram(0.1,10,4)
         self.assertEqual(12, len(pgram), "Output PGram has correct number of data points")
 
 if __name__ == "__main__":
