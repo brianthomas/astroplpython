Make fixes to finally do correct numerical LSP
calculation. Start looking at separate types of measurements

astroplpython/function/signal/LSPeriodogram.py
@@ -5,11 +5,12 @@ Created on Jul 11, 2014
 @author: thomas
 '''
 
-from astroplpython.data.PowerFrequencyMeasurement import p_f
-from astroplpython.data.TimeMeasurement import x_t
 import logging
-import scipy.signal as sp 
+
+from astroplpython.data.PowerFrequencyMeasurement import p_f
 import numpy as np
+import scipy.signal as sp 
+
 
 class LSPeriodogram(object):
     
@@ -20,47 +21,50 @@ class LSPeriodogram(object):
         ''' 
         Lomb Scargle Periodogram implementation using scipy. 
         '''
-            
-        #import sys
-        #logging.basicConfig( stream=sys.stderr )
-        log = logging.getLogger( "astroplpython.function.signal" )   
-        #log.setLevel (logging.WARN)
+
+        log = logging.getLogger("astroplpython.function.signal")   
          
         log.debug("LSPeriodogram.calculate() called")
         
         log.debug("Prepare the data; convert x_t[] to python.numpy and scale");
         x = []
         t = []
-        for v in x_t_list:
-            x.append(v.time)
-            t.append(v.value)
+        for val in x_t_list:
+            x.append(val.value)
+            t.append(val.time)
             
         # capture values as ndarray and scale
         x_arr = np.asarray(x)
+        
+        # scale the magnitude values
         x_arr = (x_arr-x_arr.mean())/x_arr.std()
         
         # capture times as ndarray 
         t_arr = np.asarray(t)
-            
+ #           
         'TODO: calculate this value ??'
         #f_low = 0.01 
         'TODO: calculate this value ??'
         #f_high = 10. 
         
         log.debug("calculate list of frequencies to use")
-        num_out = f_over * len(x)
+        num_out = len(x_t_list) * f_over
         log.debug(" Number of frequency bins out:"+str(num_out))
-        f_bins = np.linspace(f_low, f_high, num_out)
+        freqs = np.linspace(f_low, f_high, num_out)
         
         log.debug("Do pgram calculation")
-        pgram = sp.lombscargle(x_arr, t_arr, f_bins)
+        pgram = sp.lombscargle(t_arr, x_arr, freqs)
         
+        max_freq = freqs[np.argmax(pgram)] / np.pi
+        log.debug("Max Freq:"+str(max_freq))
+                
         log.debug("convert result to form we may use in db, p_f[]"); 
         p_f_list = []
         for i in range (0, num_out): 
-            p_f_list.append(p_f(pgram[i]/LSPeriodogram.TWO_PI,f_bins[i]))
+#            p_f_list.append(p_f(pgram[i]/LSPeriodogram.TWO_PI,f_bins[i]))
+            p_f_list.append(p_f(pgram[i], freqs[i]))
             
-        #log.debug("PGRAM shape:"+str(pgram.shape))
+        # log.debug("PGRAM shape:"+str(pgram.shape))
         
         return p_f_list
     

tests/astroplpython/function/signal/test_LSPeriodogram.py
@@ -16,30 +16,36 @@ class TestLSPeriodogramTestCase (unittest.TestCase):
 
     def tearDown(self):
         pass
-
-
-    def test_basic (self):
-        
-        #logging.basicConfig( stream=sys.stderr ) 
-        #log = logging.getLogger( "astroplpython.function.signal" )
-        #log.setLevel (logging.DEBUG)
-#        logging.getLogger( "astroplpython.function.signal" ).setLevel(logging.DEBUG) 
+    
+    def test_calculate (self):
+       
+        ''' 
+        test calculate static method
+        '''
+        import logging 
+        import sys
+        logging.basicConfig(stream=sys.stderr)
+        logging.getLogger( "astroplpython.function.signal" ).setLevel(logging.DEBUG) 
 
         import numpy as np
+        
+        f_low = 0.01
+        f_high = 3.0
+        f_over = 30
         
         # generates 100 evenly spaced points between 1 and 1000
         time = np.linspace(1, 1000, 100)
 
         # computes the sine value of each of those points
-        mags = np.sin(2 * time)
+        mags = np.sin(time)
         
         data = []
         i = 0
-        for m in mags:
-            data.append(x_t(m,time[i])) 
+        for t in time:
+            data.append(x_t(mags[i],t)) 
             i = i+1
         
-        p_f_list = LSPeriodogram.calculate(data,0.1,10.0,4)
+        p_f_list = LSPeriodogram.calculate(data, f_low, f_high, f_over)
         
         # get testable 
         plist = []
@@ -48,20 +54,18 @@ class TestLSPeriodogramTestCase (unittest.TestCase):
         
         pgram = np.asarray(plist)
             
-        # returns the inverse of the frequence (i.e. the period) of the largest periodogram value
+        #self.assertEqual((nr_points*f_over), len(pgram), "Output PGram has correct number of data points")
+        
+        # returns the inverse of the frequency (i.e. the period) of the largest periodogram value
         # generates 1000 frequencies between 0.01 and 1
-        freqs = np.linspace(0.01, 3, 3000)
-#        print "1/pi" + str(1/(np.pi))
-        max_freq = freqs[np.argmax(pgram)]
+#        freqs = np.linspace(f_low, f_high, 3000) #nr_points*f_over)
+        freqs = np.linspace(0.01, 3.0, 3000) #nr_points*f_over)
+        max_freq = freqs[np.argmax(pgram)] / np.pi
+        
         print("Frequency = " + str(max_freq))
+        print(" 1/pi = " + str(1/np.pi))
 
-        self.assertEqual(max_freq, (1/np.pi), "Peak power freq as expected")
-        
-#        freqs = np.linspace(0.01, 1, 1000)
-#        test = 1/freqs[np.argmax(pgram)]
-#        print ("TEST: "+test)
-        
-        self.assertEqual(12, len(pgram), "Output PGram has correct number of data points")
+        self.assertEqual(round(max_freq, 4), round((1/np.pi), 4), "Peak power freq as expected")
 
 if __name__ == "__main__":
     import sys; sys.argv = ['', 'TestLSPeriodogram.testBasic']